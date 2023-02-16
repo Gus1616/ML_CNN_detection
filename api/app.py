47455764f287e072b77b25d7d9349be26ee8e38b
Gus1616 <90276026+Gus1616@users.noreply.github.com>
2023-02-16 23:25:21 +0000

    updates

--- api/app.py
+++ api/app.py
@@ -1,14 +1,26 @@
 from flask import Flask
 from flask import request
 import tensorflow as tf
+from tensorflow import keras
+
 from flask_cors import CORS
 from flask_restful import Resource, Api, reqparse
 import os
+import cv2
+import pandas as pd
 
+import numpy as np
+from keras.models import load_model
 
 app = Flask("DetectionApi")
 CORS(app)
 api = Api(app)
+
+
+filepath = './saved_model'
+
+model = load_model(filepath, compile = True)
+
 
 class Detection(Resource):
 
@@ -26,11 +38,33 @@ class PhotoUpload(Resource):
             filename = photo.filename
             file_path = os.path.join('C:/Users/gusbo/Downloads', filename)
             photo.save(file_path)
+            image_files = pd.DataFrame(columns=range(784)).add_prefix('pixels_')
+            r_image = cv2.imread(file_path)
+            numpy_image = cv2.cvtColor(r_image, cv2.COLOR_BGR2GRAY)
+            image = cv2.resize(numpy_image, (28, 28)).astype(np.float32)
+            image = image.reshape(-1)
+            image_files.loc[photo, 'pixels_0':] = image
+            image_files = image_files / 255
+            test1 = image_files.to_numpy()
+            test1 = test1.reshape(-1, 28, 28, 1)
+            test1 = np.asarray(test1).astype('float32')
+            predictions = model.predict(test1)
+            classes = np.argmax(predictions, axis = 1)
+            label_mapping = {
+                                0: 'akiec',
+                                1: 'mel',
+                                2: 'bkl',
+                                3: 'bcc',
+                                4: 'nv',
+                                5: 'vasc',
+                                6: 'df'
+                            }
+            
 
-            # Save reference to photo in database or memory
-            # ...
 
-            return {'message': 'Photo uploaded successfully'}, 200
+
+          
+            return {'message': label_mapping[classes[0]]}, 200
         else:
             return {'message': 'No photo was provided'}, 400
 
